83df95332c2bb72f08a2e7dd48f6b4e4fba37c25
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-17 11:44:52 +0200

    commenti su ricerca.js

--- static/ricerca.js
+++ static/ricerca.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', function(){
     const opere = document.querySelectorAll('.riga_opera');
 
     barra.addEventListener('input', function(){
-        const testo_cercato = barra.value.toLowerCase();
+        const testo_cercato = barra.value.toLowerCase();    /* prende il testo dalla barra di ricerca (in minuscolo) */
 
-        opere.forEach(function(opera){
-            const testo_opera = opera.innerText.toLowerCase();
+        opere.forEach(function(opera){  /* cicla sulle opere */
+            const testo_opera = opera.innerText.toLowerCase();  /* prende il testo dalle opere (in minuscolo) */
 
-            if(testo_opera.includes(testo_cercato)){
+            if(testo_opera.includes(testo_cercato)){    // se l'opera contiene il testo_cercato allora non fa nulla
                 opera.style.display = '';
             }
-            else{
+            else{                               // altrimenti non la visualizza (display='none')
                 opera.style.display = 'none';
             }
         });
