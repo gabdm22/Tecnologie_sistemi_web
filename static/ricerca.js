99c0f242e18962800eb784d5e8d3e820a6a47844
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-19 15:16:14 +0200

    script per filtro categorie

--- static/ricerca.js
+++ static/ricerca.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
     const barra = document.getElementById('barra-ricerca');
+    const filtro = document.getElementById('filtro-categoria');
     const opere = document.querySelectorAll('.riga_opera');
 
-    barra.addEventListener('input', function(){
+    function aggiornaVetrina(){
         const testo_cercato = barra.value.toLowerCase();    /* prende il testo dalla barra di ricerca (in minuscolo) */
+        const cat_cercata = filtro.value.toLowerCase();
 
         opere.forEach(function(opera){  /* cicla sulle opere */
-            const testo_opera = opera.innerText.toLowerCase();  /* prende il testo dalle opere (in minuscolo) */
+            const titolo = opera.querySelector('.nome_opera').innerText.toLowerCase();  /* prende il testo dalle opere (in minuscolo) */
 
-            if(testo_opera.includes(testo_cercato)){    // se l'opera contiene il testo_cercato allora non fa nulla
+            const dettagli = opera.querySelectorAll('.dettaglio');  // i dettagli sono autore [0], categoria [1], dimensioni[2]
+            const autore = dettagli[0].innerText.toLowerCase();
+            const categoria = dettagli[1].innerText.toLowerCase();
+
+            const testo_ok = (testo_cercato==='') || titolo.includes(testo_cercato) || autore.includes(testo_cercato);
+            const categoria_ok = (cat_cercata==='tutte') || categoria.includes(cat_cercata);
+
+            if(testo_ok && categoria_ok){    // se l'opera contiene il testo_cercato allora non fa nulla
                 opera.style.display = '';
             }
             else{                               // altrimenti non la visualizza (display='none')
                 opera.style.display = 'none';
             }
         });
-    });
+    }
+    
+    barra.addEventListener('input', aggiornaVetrina);
+    filtro.addEventListener('change', aggiornaVetrina);
 });
